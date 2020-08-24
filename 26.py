7a40c0da639db339991b04d86e5d890557826231
Ari Hamdani <aryham04@gmail.com>
2020-08-24 17:51:07 +0900

    Update 26.py

--- 26.py
+++ 26.py
@@ -15,6 +15,8 @@ y1= x**2
 plt.plot(x, y)
 plt.plot(x, y1)
 """
+#plot histogram
+"""
 from skimage import io, img_as_ubyte
 
 img_gray = io.imread('images/test_image.jpg', as_gray=True)
@@ -22,3 +24,13 @@ img_gray_8bit = img_as_ubyte(img_gray)
 
 #plt.imshow(img_gray, cmap='gray')
 plt.hist(img_gray_8bit.flat,bins=100, range=(0,255) )
+"""
+
+#plot line and symbols
+a=np.array([1,2,3,4,5])
+b=np.array([1,4,9,16,25])
+plt.plot(a, b)
+plt.plot(a,b,'bo') #blue dots. Also try:'r--' 'g^'
+plt.axis([0,6,0,50])
+plt.xlabel('x')
+plt.ylabel('y')
